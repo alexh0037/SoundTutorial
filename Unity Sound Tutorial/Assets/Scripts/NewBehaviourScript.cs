f04b50c7f1f27f071aa08ae4cb114c535a573a10
alexh0037 <alexh0037@users.noreply.github.com>
2020-10-04 18:08:01 -0400

    Animation commit

--- Unity Sound Tutorial/Assets/Scripts/NewBehaviourScript.cs
+++ Unity Sound Tutorial/Assets/Scripts/NewBehaviourScript.cs
@@ -10,11 +10,14 @@ public class NewBehaviourScript : MonoBehaviour
 
     public AudioClip musicClipTwo;
 
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -23,19 +26,27 @@ public class NewBehaviourScript : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State", 1);
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
         }
     }
 }
